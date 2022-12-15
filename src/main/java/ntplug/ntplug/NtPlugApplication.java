1e27d501868fb50d2b4653f6ae6ace6f655fe6d2
frisko-sposad <safiko@ya.ru>
2022-12-15 10:31:18 +0300

    init commit

---
+++ src/main/java/ntplug/ntplug/NtPlugApplication.java
@@ -0,0 +1,13 @@
+package ntplug.ntplug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtPlugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtPlugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntplug/ntplug/NtPlugApplicationTests.java
@@ -0,0 +1,13 @@
+package ntplug.ntplug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtPlugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
