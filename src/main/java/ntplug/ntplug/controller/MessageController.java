d541be1d4898af783667a209a2284c05ade18a46
frisko-sposad <safiko@ya.ru>
2022-12-15 16:35:32 +0300

    added User and UserPost classes

--- src/main/java/ntplug/ntplug/controller/MessageController.java
+++ src/main/java/ntplug/ntplug/controller/MessageController.java
@@ -2,33 +2,18 @@ package ntplug.ntplug.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("type","Get"); put("date", String.valueOf(new Date())); }});
-    }};
-
-    SimpleDateFormat formatForTimeNow = new SimpleDateFormat("'Текущее время' hh:mm:ss");
-    String time =  formatForTimeNow.format(new Date());
-
-    @GetMapping("get")
-    public List<Map<String, String>> list() {
-        return Collections.singletonList(new HashMap<String, String>() {{
-            put("type", "Get");
-            put("date", time);
-        }});
+    @GetMapping("/user")
+    User getUser() {
+        return new User("login", "password");
     }
 
-    @PostMapping("post")
-    public Map<String, String> create(@RequestBody Map<String, String> message) {
-
-        String login = message.get("login");
-        return new HashMap<String, String>() {{ put("login", login); put("date", String.valueOf(time)); }};
+    @PostMapping("/user")
+    UserPost postUser(@RequestBody User user) {
+        return new UserPost(user.getLogin(), String.valueOf(new Date()));
     }
 }
-
-//fetch('/message/post', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({login: "login", pass: "pass"})}).then(result => console.log(result))
