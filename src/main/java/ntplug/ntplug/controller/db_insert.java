7be1d172ab6e30774e4d58526e108f84fedb9c65
frisko-sposad <safiko@ya.ru>
2023-01-02 01:30:50 +0300

    added insert

---
+++ src/main/java/ntplug/ntplug/controller/db_insert.java
@@ -0,0 +1,41 @@
+package ntplug.ntplug.controller;
+
+//STEP 1. Import required packages
+        import java.sql.*;
+        import java.util.Date;
+        import java.util.Scanner;
+
+public class db_insert {
+
+    //  Database credentials
+    static final String DB_URL = "jdbc:postgresql://192.168.1.8:5432/postgres";
+    static final String USER = "postgres";
+    static final String PASS = "postgres";
+
+    public static void main(String[] argv) throws SQLException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your login: ");
+        String login = scanner.nextLine();
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+        System.out.println("Login= " + login + ", Password= " + password);
+
+// Создание соединения с базой данных, оператора доступа к базе данных,выполнение запроса к базе данных, получение набора данных
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement statement  = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(
+                     String.format(
+                             "INSERT INTO public.users (login, password, date) VALUES('" + login +"', '"+ password +"', '"+ new Date()+"');"
+                     )
+             );
+                ) {
+                System.out.println("Данные добавлены");
+
+        } catch (SQLException e) {
+            System.out.println("Error");
+        }
+
+
+    }
+}
--- src/main/java/ntplug/ntplug/controller/db_select.java
+++ src/main/java/ntplug/ntplug/controller/db_select.java
@@ -12,35 +12,6 @@ public class db_select {
 
     public static void main(String[] argv) throws SQLException {
 
-//        System.out.println("Testing connection to PostgreSQL JDBC");
-//
-//        try {
-//            Class.forName("org.postgresql.Driver");
-//        } catch (ClassNotFoundException e) {
-//            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
-//            e.printStackTrace();
-//            return;
-//        }
-//
-//        System.out.println("PostgreSQL JDBC Driver successfully connected");
-//        Connection connection = null;
-//
-//        try {
-//            connection = DriverManager
-//                    .getConnection(DB_URL, USER, PASS);
-//
-//        } catch (SQLException e) {
-//            System.out.println("Connection Failed");
-//            e.printStackTrace();
-//            return;
-//        }
-//
-//        if (connection != null) {
-//            System.out.println("You successfully connected to database now");
-//        } else {
-//            System.out.println("Failed to make connection to database");
-//        }
-
         try {
             // Создание соединения с базой данных
             Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
